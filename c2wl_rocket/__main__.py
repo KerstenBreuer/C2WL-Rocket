8e723963e9f570c17bb3626e273d58143d140d81
KerstenBreuer <kersten-breuer@outlook.com>
2020-02-23 21:18:59 +0100

    implement call of cwltool main function

--- c2wl_rocket/__main__.py
+++ c2wl_rocket/__main__.py
@@ -1,5 +1,14 @@
 from __future__ import absolute_import
 import argparse
+import cwltool.main
+import cwltool.argparser
+from cwltool.executors import MultithreadedJobExecutor, SingleJobExecutor
+from .tool_runner import make_custom_tool
+from copy import copy
+
+## get cwltool default args:
+cwltool_ap = cwltool.argparser.arg_parser()
+cwltool_default_args = cwltool_ap.parse_args([])
 
 def main():
     parser = argparse.ArgumentParser(
@@ -18,11 +27,29 @@ def main():
     parser.add_argument('input_params',
         help="Provide input parameters in YAML or JSON format."
     )
+    
+    parsed_args = parser.parse_args()
 
-    args = parser.parse_args()
+
+    
+    args = copy(cwltool_default_args)
+    args.workflow = parsed_args.cwl_document
+    args.job_order = parsed_args.input_params
+
+    loading_context = cwltool.main.LoadingContext(vars(args))
+    loading_context.construct_tool_object = make_custom_tool
+    runtime_context = cwltool.main.RuntimeContext(vars(args))
+    job_executor = MultithreadedJobExecutor() if args.parallel \
+        else SingleJobExecutor()
+    job_executor.max_ram = job_executor.max_cores = float("inf")
     
     # hand arguments over to main exec function:
-    pass
+    cwltool.main.main(
+        args=args,
+        executor=job_executor,
+        loadingContext=loading_context,
+        runtimeContext=runtime_context
+    )
 
 if __name__ == "__main__":
     main()
