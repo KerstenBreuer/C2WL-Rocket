c761a07499cdc4cdb9e8ba35832bc463f63a6900
KerstenBreuer <kersten-breuer@outlook.com>
2020-03-29 17:40:15 +0200

    test entry points from api and command line

--- tests/integration/test_local_exec.py
+++ tests/integration/test_local_exec.py
@@ -3,51 +3,50 @@ import pytest
 import argparse
 import shutil
 import subprocess
-
-dir_of_this_script = os.path.dirname(os.path.realpath(__file__))
-test_out_dir = os.path.join(dir_of_this_script, "../../test_out")
-fixtures_dir = os.path.join(dir_of_this_script, "fixtures")
-c2wl_cmdl_entry_point = os.path.join(dir_of_this_script, "../../c2wl_rocket.py")
-
-sys.path.append(os.path.join(dir_of_this_script, "..", ".."))
+from . import test_out_dir, package_root, fixtures_dir, \
+    c2wl_cmdl_entry_point, dir_of_this_script
+sys.path.append(package_root)
+from .exec_profiles import LocalToolExec
 import c2wl_rocket.__main__
 
 def get_job(name_cwl, name_input=None):
     name_input = name_input if name_input is not None else name_cwl
-    input = os.path.join(fixtures_dir, f"{name_input}.yaml") 
+    inputs = os.path.join(fixtures_dir, f"{name_input}.yaml") 
     cwl = os.path.join(fixtures_dir, f"{name_cwl}.cwl") 
-    return cwl, input
+    success_expected = "fail" not in name_cwl and "fail" not in name_input
+    return cwl, inputs, success_expected
 
 
 @pytest.mark.parametrize(
-    "cwl, inputs", 
+    "cwl, inputs, success_expected", 
     [
         get_job("touch")
     ]
 )
-def test_succeeding_jobs(cwl, inputs):
+def test_api_entry_point_jobs(cwl, inputs, success_expected):
     c2wl_rocket.__main__.run(
         cwl, 
         inputs, 
         outdir=test_out_dir,
+        exec_profile=LocalToolExec,
         debug=True
     )
-    shutil.rmtree(test_out_dir)
-
 
 
 @pytest.mark.parametrize(
-    "cwl, inputs", 
+    "cwl, inputs, success_expected", 
     [
-        get_job("touch_fail")
+        get_job("touch")
     ]
 )
-def test_failing_jobs(cwl, inputs):
+def test_cmdl_entry_point_jobs(cwl, inputs, success_expected):
     p = subprocess.Popen(
             [
                 "python3",
                 c2wl_cmdl_entry_point,
                 "--debug",
+                "--exec-profile",
+                os.path.join(dir_of_this_script, "exec_profiles.py") + ":LocalToolExec"
                 "--outdir",
                 test_out_dir,
                 cwl,
@@ -57,7 +56,22 @@ def test_failing_jobs(cwl, inputs):
         )
     exit_code = p.wait()
     stderr = str(p.stderr.read()).split("\\n")
-    assert stderr[-2] == "Final process status is permanentFail", \
-        "Final process status is not permanentFail."
-    assert exit_code != 0, "Test passed but it should have failed."
+    
+    # check final status message in log:
+    final_status = stderr[-2]
+    assert "Final process status" in final_status, \
+        f"No final process status detected in log."
+    if success_expected:
+        assert final_status == "Final process status is success", \
+            f"Success expected but final status message was: \"{final_status}\""
+    else:
+        assert final_status == "Final process status is permanentFail", \
+            f"Fail expected but final status message was: \"{final_status}\""
+    
+    # check exit code:
+    if success_expected:
+        assert exit_code == 0, "Success expected but exit code is non-zero."
+    else:
+        assert exit_code != 0, "Fail expected but exit code is zero."
+
 
